a253456df9d60c88691e7aaeb73cce8cef9ef087
Vilém Zouhar <vilem.zouhar@gmail.com>
2025-02-17 18:13:02 +0100

    add tests for more or less than 100 items; add info about the number of loaded items

--- EvalData/models/data_assessment.py
+++ EvalData/models/data_assessment.py
@@ -425,6 +425,7 @@ class DataAssessmentTask(BaseMetadata):
                 )
                 new_items.append(new_item)
 
+            LOGGER.info(f'The task has {len(new_items)} items')
             current_count += 1
 
             # for new_item in new_items:
