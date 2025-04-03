a98b026ca0a5bb862a87a6251e3e093a96dc8212
Hiba El Oirghi <eloirghi@clipwww02.umiacs.umd.edu>
2025-04-03 12:37:23 -0400

    Added umiacs to allowed hosts

--- Appraise/settings.py
+++ Appraise/settings.py
@@ -35,7 +35,8 @@ if SECRET_KEY == _SECRET_KEY_DEFAULT:
         'Using the default SECRET_KEY value! Set and export APPRAISE_SECRET_KEY envvar instead'
     )
 
-ALLOWED_HOSTS = os.environ.get('APPRAISE_ALLOWED_HOSTS', '127.0.0.1').split(',')
+#ALLOWED_HOSTS = os.environ.get('APPRAISE_ALLOWED_HOSTS', '127.0.0.1').split(',')
+ALLOWED_HOSTS = ['wiki-annotation.umiacs.umd.edu', 'localhost', '127.0.0.1']
 
 CSRF_TRUSTED_ORIGINS = os.environ.get('APPRAISE_CSRF_TRUSTED_ORIGINS', 'https://*.127.0.0.1').split(',')
 
